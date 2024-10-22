12546dc5251f91494ddef6fa9a045e544abc84ea
Roma Radionov <radionroman@gmail.com>
2024-10-22 19:03:56 +0200

    Added list_regex

--- script.py
+++ script.py
@@ -34,19 +34,37 @@ def upload_file(file_name, bucket_name, object_name):
         print(f"The file {file_name} was not found.")
     except NoCredentialsError:
         print("Credentials not available.")
+        
+def list_files_with_regex(bucket_name, prefix, pattern):
+    try:
+        response = s3.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
+        if 'Contents' in response:
+            for obj in response['Contents']:
+                if re.search(pattern, obj['Key']):
+                    print(obj['Key'])
+        else:
+            print("No files found.")
+    except ClientError as e:
+        print(f"Error: {e}")
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='List files in the bucket or upload a file to the bucket')
-    parser.add_argument('operation', type=str, help='list or upload')
-    parser.add_argument('--file_name', type=str, help='Name of the file to upload')
+    parser = argparse.ArgumentParser(description="S3 CLI using boto3")
+    parser.add_argument('action', choices=['list', 'upload', 'list_regex'], help='Action to perform')
+    parser.add_argument('--file', help='File to upload')
+    parser.add_argument('--object', help='S3 object key for upload')
+    parser.add_argument('--pattern', help='Regex pattern to filter files')
+
     args = parser.parse_args()
-    if args.operation == 'list':
+
+    if args.action == 'list':
         list_files(bucket_name, prefix)
-    elif args.operation == 'upload':
-        if args.file_name:
-            object_name = prefix + os.path.basename(args.file_name)
-            upload_file(args.file_name, bucket_name, object_name)
+    elif args.action == 'upload':
+        if args.file and args.object:
+            upload_file(args.file, bucket_name, f"{prefix}{args.object}")
         else:
-            print("Please provide the name of the file to upload.")
-    else:
-        print("Invalid operation. Please use 'list' or 'upload'.")
+            print("Please provide --file and --object for uploading.")
+    elif args.action == 'list_regex':
+        if args.pattern:
+            list_files_with_regex(bucket_name, prefix, args.pattern)
+        else:
+            print("Please provide --pattern for listing files with regex.")
